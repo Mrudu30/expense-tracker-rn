d0bae188e9988343b1b3f504579e4e8bb3e2b1ac
Mrudani Songade <122159811+Mrudu30@users.noreply.github.com>
2024-07-12 15:05:29 +0530

    added all the fixtures

---
+++ constants/styles.js
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: '#E0F2F1',
+    primary100: '#B2DFDB',
+    primary200: '#80CBC4',
+    primary400: '#26A69A',
+    primary500: '#009688',
+    primary700: '#00796B',
+    primary800: '#004D40',
+    accent500: '#4CAF50',
+    error50: '#FFCDD2',
+    error500: '#D32F2F',
+    gray500: '#757575',
+    gray700: '#424242',
+  },
+};
